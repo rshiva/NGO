f0202f376ee410d6459c4aeac9bb67785784757f
rshiva <nadarshiva.r@gmail.com>
2013-11-24 21:02:12 +0530

    initial commit with devise gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelpNgo::Application.config.secret_key_base = '520f506d6435a71b2b6a2550a4dc7c1470c93fafc31f74f0dff212107df70113d37c9c125fedd25e3ee93b0e73718e2b8fed57fd0cebaf5bf51c94fd7fac3034'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HelpNgo::Application.config.session_store :cookie_store, key: '_help_ngo_session'
